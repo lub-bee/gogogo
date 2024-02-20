91d4dfad3853d797a8e47634851de6eda47e0e21
Mahul Patel <mahulpatel@Mahuls-MacBook-Air.local>
2024-02-20 21:45:59 +0900

    Add model relation to user

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        "start_at" => "date",
+    ];
+
+    public function user(): BelongsTo
+    {
+
+/*
+ how to use in the view?
+ $model->relatedtable()-> ...
+ this comes before the database request
+
+ $model->relatedtable->property
+ this comes after the database request
+ this gives you the model entity/property which you can use or see easily.
+ */
+
+
+        return $this->belongsTo(User::class);
+    }
 }
