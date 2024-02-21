1c5384c03571937dddf265ea9ecf62b27c28fdf7
Mahul Patel <mahulpatel@Mahuls-MacBook-Air.local>
2024-02-22 00:19:09 +0900

    Location Controller cleaner and  updated with fields and format

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,40 +3,65 @@
 namespace App\Http\Controllers;
 
 use App\Models\Location;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class LocationController extends Controller
 {
-    function index() : View
+    /**
+     * Display location list
+     *
+     * @return View
+     */
+    public function index() : View
     {
-        //$locations = Location::all();
-        $locations = Location::get();
+        $locations = Location::orderBy("cost", "desc")
+            ->orderBy("name", "asc")->get();
 
         return view("admin.location.index")
             ->with("locations", $locations);
 
     }
 
-    function show(int $location_id) : View
+    /**
+     * Display one specific location
+     *
+     * @return View
+     */
+    public function show(int $location_id) : View
     {
         $location = Location::findOrFail($location_id);
+
         return view("admin/location/show")
            ->with("location", $location);
 
     }
 
+/**
+ * Display the create form
+ */
     function create() : View
     {
         return view("admin.location.create");
 
     }
 
+    /**
+     * Store a new location
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function store(Request $request)
     {
         $validated = $request->validate([
-            "name" => "required|string|max:255",
+            "name" => [
+                "required",
+                "string",
+                "max:255"
+            ],
             "description_en" => [
                 "nullable",
                 "string",
@@ -47,51 +72,72 @@ class LocationController extends Controller
                 "string",
                 "max:2000"
             ],
+            "gps_long" => [
+                "nullable",
+                "string",
+                "max:15"
+            ],
+            "gps_lat" => [
+                "nullable",
+                "string",
+                "max:15"
+            ],
+            "website_url" => [
+                "nullable",
+                "string",
+                "max:200"
+            ],
             //TO CHECK - Default value vs Nullable.
             "cost" => [
                 "nullable",
-                //"string",
-                //"max:1000"
+                "string",
+                "max:100000"
             ]
-
         ]);
 
         $location = new Location();
         $location->name = $validated["name"];
         $location->description_en = $validated["description_en"];
         $location->description_ja = $validated["description_ja"];
+        $location->gps_long = $validated["gps_long"];
+        $location->gps_lat = $validated["gps_lat"];
+        $location->website_url = $validated["website_url"];
         $location->cost = $validated["cost"];
-
-
-
         $location->user_id = auth()->user()->id;
-
         $location->save();
-        return redirect()->route("location.show", $location->id);
-        //return response()->redirect();
 
+        return redirect(route("location.index"))
+            ->with("success", "Location saved successfully");
     }
 
-    function edit($location_id) : View
+    /**
+     * Display the edit form
+     *
+     * @param int $location_id
+     * @return View
+     */
+    public function edit(int $location_id) : View
     {
-        $location = Location::where('id',$location_id)->firstOrFail();
-
-        // $location = Location::find($location_id);
-        //by author
-
-        // $location = "location";
-
-        // dd($location_id);
+        $location = Location::findOrFail($location_id);
         return view("admin.location.edit")
-            ->with('location_id', $location_id)
             ->with('location', $location);
-
     }
 
-    function update($request,$location_id) : Response
+    /**
+     * Update a specific location
+     *
+     * @param int $location_id
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function update(int $location_id, Request $request): RedirectResponse
     {
         $validated = $request->validate([
-            "name" => "required|string|max:255",
+            "name" => [
+                "required",
+                "string",
+                "max:255"
+            ],
             "description_en" => [
                 "nullable",
                 "string",
@@ -102,33 +148,58 @@ class LocationController extends Controller
                 "string",
                 "max:2000"
             ],
+            "gps_long" => [
+                "nullable",
+                "string",
+                "max:15"
+            ],
+            "gps_lat" => [
+                "nullable",
+                "string",
+                "max:15"
+            ],
+            "website_url" => [
+                "nullable",
+                "string",
+                "max:200"
+            ],
             //TO CHECK - Default value vs Nullable.
             "cost" => [
                 "nullable",
-                "enum",
-                "max:1000"
+                "string",
+                "max:100000"
             ]
-
         ]);
 
-        $location = Location::findOrFail($location_id);
+        $location = Location::find($location_id);
         $location->name = $validated["name"];
         $location->description_en = $validated["description_en"];
         $location->description_ja = $validated["description_ja"];
+        $location->gps_long = $validated["gps_long"];
+        $location->gps_lat = $validated["gps_lat"];
+        $location->website_url = $validated["website_url"];
         $location->cost = $validated["cost"];
 
+        $location->save();
 
+        //  TO CHECK - $location->user_id = auth()->user()->id;
 
-        $location->user_id = auth()->user()->id;
-
-        return response()->redirect();
-
+        return redirect(route("location.show", $location->id))
+            ->with("success", "Event saved successfully");
     }
 
-    function delete($request) : Response
+    /**
+     * Delete a specific location
+     * todo
+     *
+     * @param int $location_id
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function delete(Request $request, int $location_id): RedirectResponse
     {
-
-        return response()->redirect();
-
+        //TODO
+        return redirect(route("location.index"))
+            ->with('success', "Location deleted successfully");
     }
 }
