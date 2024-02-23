3f9c8749917298647a12feec624ccc8d1b54436a
Mahul Patel <mahulpatel@Mahuls-MacBook-Air.local>
2024-02-23 14:33:00 +0900

    Topic Controller Formatted and corrected

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -10,34 +10,107 @@ use Illuminate\View\View;
 
 class TopicController extends Controller
 {
-    function index() : View
+    /**
+     * Display topic list
+     *
+     * @return View
+     */
+    public function index() : View
     {
-        //$topics = Topic::all();
-        $topics = Topic::get();
+        $topics = Topic::orderBy("updated_at","desc")
+            ->orderBy('name', 'asc')->get();
 
         return view("admin.topic.index")
             ->with("topics", $topics);
-
     }
 
-    function show(int $topic_id) : View
+    /**
+     * Display one specifc topic detail
+     *
+     * @return View
+     */
+    public function show(int $topic_id) : View
     {
         $topic = Topic::findOrFail($topic_id);
         return view("admin/topic/show")
            ->with("topic", $topic);
-
     }
 
-    function create() : View
+    /**
+     * Display create form
+     *
+     * @return View
+     */
+    public function create() : View
     {
         return view("admin.topic.create");
-
     }
 
-    function store(Request $request) : RedirectResponse
+    /**
+     * Store new topic instance
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request) : RedirectResponse
     {
         $validated = $request->validate([
-            "name" => "required|string|max:60",
+            "name" => [
+                "required",
+                "string",
+                "max:60"
+            ],
+            "description_en" => [
+                "nullable",
+                "string",
+                "max:2000"
+            ],
+            "description_ja" => [
+                "nullable",
+                "string",
+                "max:2000"
+            ],
+        ]);
+
+        $topic = new Topic();
+        $topic->name = $validated["name"];
+        $topic->description_en = $validated["description_en"];
+        $topic->description_ja = $validated["description_ja"];
+        $topic->user_id = auth()->user()->id;
+        $topic->save();
+
+        return redirect()->route("topic.index")
+            ->with("success", "Topic saved successfully");
+    }
+
+    /**
+     * Display edit form
+     *
+     * @param int $topic_id
+     * @return View
+     */
+    public function edit(int $topic_id) : View
+    {
+        $topic = Topic::findOrFail($topic_id);
+        return view("admin.topic.edit")
+            ->with('topic', $topic);
+    }
+
+    /**
+     * Update a specific Topic
+     *
+     * @param int $topic_id
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function update(int $topic_id , Request $request) : RedirectResponse
+    {
+        $validated = $request->validate([
+            "name" => [
+                "required",
+                "string",
+                "max:60"
+            ],
             "description_en" => [
                 "nullable",
                 "string",
@@ -55,40 +128,23 @@ class TopicController extends Controller
         $topic->description_en = $validated["description_en"];
         $topic->description_ja = $validated["description_ja"];
 
-        $topic->user_id = auth()->user()->id;
-
         $topic->save();
 
-        return redirect()->route("topic.index");
-
+        return redirect(route("topic.show", $topic->id))
+            ->with("success", "Topic saved successfully");
     }
-
-    function edit($topic_id) : View
+    /**
+     * Delete a specific topic
+     * todo
+     *
+     * @param int $topic_id
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function delete(int $topic_id, Request $request) : RedirectResponse
     {
-        $topic = Topic::where('id',$topic_id)->firstOrFail();
-
-        // $topic = Topic::find($topic_id);
-        //by author
-
-        // $topic = "topic";
-
-        // dd($topic_id);
-        return view("admin.topic.edit")
-            ->with('topic_id', $topic_id)
-            ->with('topic', $topic);
-
-    }
-
-    function update($request) : Response
-    {
-        return response()->redirect();
-
-    }
-
-    function delete($request) : Response
-    {
-
-        return response()->redirect();
-
+        //todo
+        return redirect(route("topic.index"))
+        ->with('success','Event deleted successfully');
     }
 }
