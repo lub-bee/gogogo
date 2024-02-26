92187a41db99c67b4a19d0ebdc6cb9a6df0c5854
Mahul Patel <mahulpatel@Mahuls-MacBook-Air.local>
2024-02-26 21:45:29 +0900

    Media and Topic Model updates

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Media extends Model
 {
     use HasFactory;
 
     protected $table = "medias";
+
+    public function user(): BelongsTo
+    {
+
+    return $this->belongsTo((User::class));
+    }
 }
