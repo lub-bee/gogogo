d449559bddf32cdc8a1f40310673c629a4ad1a3a
Mahul Patel <mahulpatel@Mahuls-MacBook-Air.local>
2024-02-27 22:03:50 +0900

    Base code for password and email addition

---
+++ app/Http/Controllers/UserEmailController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserEmailController extends Controller
+{
+    //just update - no ned for the other usual controller actions
+    //no view for this controller - only present in in user edit page
+}
