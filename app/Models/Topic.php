61076040e6085e35c323647a6db053ac2a5bd053
Mahul Patel <mahulpatel@Mahuls-MacBook-Air.local>
2024-02-29 22:20:18 +0900

    Topic Event relation added

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Topic extends Model
 {
@@ -18,5 +19,10 @@ class Topic extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo((User::class));
+    }
+
+    public function event(): HasMany
+    {
+        return $this->hasMany(Event::class);
     }
 }
