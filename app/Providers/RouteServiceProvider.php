0be7f41abe659eb96b25ead5ae761e7e5b9946ad
Ludovic Blervaque Brient <ludovic.blervaque@gmail.com>
2024-04-09 21:05:20 +0900

    Fixing broken redirection after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/management';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
