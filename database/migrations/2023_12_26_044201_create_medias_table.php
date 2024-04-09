c8ad0dfa3239d8d78ca89f4ecb3d6cdce1994c0e
Mahul Patel <mahulp9556@googlemail.com>
2024-04-09 20:03:35 +0900

    Fix relation integrity when deleted entry

--- database/migrations/2023_12_26_044201_create_medias_table.php
+++ database/migrations/2023_12_26_044201_create_medias_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text("description_ja")->nullable();
             $table->timestamps();
 
-            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("user_id")->constrained("users")->onDelete("set null");
 
         });
     }
