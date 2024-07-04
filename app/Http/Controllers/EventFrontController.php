881c94cba78376548b7af8e5a6db87673beac509
Mahul Patel <mahulp9556@googlemail.com>
2024-07-04 21:51:16 +0900

    front event show and controller working

--- app/Http/Controllers/EventFrontController.php
+++ app/Http/Controllers/EventFrontController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventFrontController extends Controller
 {
     public function index(Request $request){
+
+
         //displays list of events
         return view("front.event-index");
     }
 
     public function show(string $event_slug){
+        $event = Event::where("slug", $event_slug)->firstOrFail();
+
         //display a specific event
-        return view("front.event-show");
+        return view("front.event-show")
+            ->with("event", $event);
     }
 }
--- resources/views/front/event-show.blade.php
+++ resources/views/front/event-show.blade.php
@@ -1,1 +1,18 @@
-EVENT SHOW
+
+{{-- $event = new Event();
+$event->name = $validated["name"];
+$event->slug = $validated["slug"];
+$event->start_at = $validated['start_at'];
+$event->end_at = $validated['end_at'];
+$event->description_en = $validated["description_en"];
+$event->description_ja = $validated["description_ja"];
+$event->cost = $validated["cost"]; --}}
+
+{{$event->name}}<br>
+{{$event->slug}}<br>
+{{$event->start_at}}<br>
+{{$event->end_at}}<br>
+{{$event->description_en}}<br>
+{{$event->description_ja}}<br>
+{{$event->cost}}<br>
+
