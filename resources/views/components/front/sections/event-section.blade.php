0238f380ceb0a00476fc8f972b232c2bc49f8dab
Mahul Patel <mahulp9556@googlemail.com>
2024-09-03 22:19:34 +0900

    Date added to event front

--- resources/views/components/front/sections/event-section.blade.php
+++ resources/views/components/front/sections/event-section.blade.php
@@ -6,7 +6,7 @@
 
 <main class="flex-1 mt-4 md:mt-0 h-full flex flex-col md:flex-row items-center justify-evenly" :class="mode === 'default' ? '' : 'hidden'">
     <div class=' md:h-full md:w-auto flex flex-row md:flex-col justify-evenly items-center'>
-        <x-event.calendar-card year="2024" month="mar" day="22"/>
+        <x-event.calendar-card year="{{$event->start_at->format('Y')}}" month="{{$event->start_at->format('M')}}" day="{{$event->start_at->format('d')}}"/>
         <x-event.responsive-related-card media_id="1" topic_id="2" location_id="3"/>
     </div>
     <x-event.event-info-card/>
