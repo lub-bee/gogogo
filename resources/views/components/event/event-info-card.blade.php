79a22a53dbeff313f1965b2bdf35952c29b4faa2
Ludovic Blervaque Brient <ludovic.blervaque@gmail.com>
2024-09-03 21:51:18 +0900

    Get component event info card ready to receive data

--- resources/views/components/event/event-info-card.blade.php
+++ resources/views/components/event/event-info-card.blade.php
@@ -1,13 +1,12 @@
+@props(["title","description_en","description_ja"])
 <div class='w-full md:w-2/4'>
     <div class='xs:mt-4 px-4 lg:px-0 text-[2.5rem] xl:text-[5rem] leading-[2.5rem] xl:leading-[5rem] lg:tracking-tight font-bold uppercase'>
-        A super BBQ on Hirosegawa
+        {{ $title }}
     </div>
     <div class='px-4 lg:px-0 mt-4 lg:hover:scale-105 lg:text-lg transition-all max-h-[18vh] md:h-[30vh] overflow-y-scroll'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla numquam qui corrupti perspiciatis porro earum dignissimos voluptatibus quidem sequi libero cupiditate, aliquam ullam itaque dolores accusantium pariatur quibusdam quod saepe! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veniam quisquam possimus, optio delectus excepturi laboriosam sequi minima! Ut blanditiis consectetur minima illum odio, hic iure sit esse dolore iste!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla numquam qui corrupti perspiciatis porro earum dignissimos voluptatibus quidem sequi libero cupiditate, aliquam ullam itaque dolores accusantium pariatur quibusdam quod saepe! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veniam quisquam possimus, optio delectus excepturi laboriosam sequi minima! Ut blanditiis consectetur minima illum odio, hic iure sit esse dolore iste!
+        {!! $description_en !!}
     </div>
     <div class='px-4 lg:px-0 mt-4 lg:hover:scale-105 lg:text-lg transition-all max-h-[18vh] md:h-[30vh] overflow-y-scroll'>
-        団マヘ人毎せなゅざ南変ワスネエ塊模ム馬占ら民音ヨヤニ図小ぐきび長表代いず氏委中取イでふよ朗祭役歓げぐせ。京お周婚ぽろ無上めび年暮もぐご次校ウス結36録36尾ッぜひ員6崎へむぶ済詩ヤ間本チ毎受ワミ転族セコワ球8拐シカチ茨正ヤレ紹台謙魚禁だン。活ょレフ重社ウ高供選覧ヨニユ公読すはう東身にぴのふ援現ぴ終販フル戦見ウラクヤ皆年じあ政民イ覧学追ぼ稿人強余でべぐ。
-        団マヘ人毎せなゅざ南変ワスネエ塊模ム馬占ら民音ヨヤニ図小ぐきび長表代いず氏委中取イでふよ朗祭役歓げぐせ。京お周婚ぽろ無上めび年暮もぐご次校ウス結36録36尾ッぜひ員6崎へむぶ済詩ヤ間本チ毎受ワミ転族セコワ球8拐シカチ茨正ヤレ紹台謙魚禁だン。活ょレフ重社ウ高供選覧ヨニユ公読すはう東身にぴのふ援現ぴ終販フル戦見ウラクヤ皆年じあ政民イ覧学追ぼ稿人強余でべぐ。
+        {!! $description_ja !!}
     </div>
 </div>
