8a146d9987d95b8ae2842ffc82e17ec15bfa2fe4
Mahul Patel <mahulp9556@googlemail.com>
2024-09-03 22:30:37 +0900

    Add Event name descriptions and fix event section error

--- resources/views/front/event-show.blade.php
+++ resources/views/front/event-show.blade.php
@@ -11,9 +11,9 @@
         <main class="flex-1 mt-4 md:mt-0 h-full flex flex-col md:flex-row items-center justify-evenly" :class="mode === 'default' ? '' : 'hidden'">
             <div class=' md:h-full md:w-auto flex flex-row md:flex-col justify-evenly items-center'>
                 <x-event.calendar-card year="{{$event->start_at->format('Y')}}" month="{{$event->start_at->format('M')}}" day="{{$event->start_at->format('d')}}"/>
-                <x-event.responsive-related-card/>
+                <x-event.responsive-related-card  media_id="1" topic_id="2" location_id="3"/>
             </div>
-            <x-event.event-info-card/>
+            <x-event.event-info-card name="{{$event->name}}" description_en="{{$event->description_en}}" description_ja="{{$event->description_ja}}" />
         </main>
 
     </div>
