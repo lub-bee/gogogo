b9c92400eb573fb3635dba2e4f4a890f78a30060
Ludovic Blervaque Brient <ludovic.blervaque@gmail.com>
2024-09-04 08:25:26 +0900

    Fixing a bug blocking the format of the text

--- resources/views/components/event/event-info-card.blade.php
+++ resources/views/components/event/event-info-card.blade.php
@@ -1,12 +1,12 @@
-@props(["name"=> "dummy title","description_en"=>"dummy description en","description_ja"=>"dummy description ja"])
+@props(["event"=> null, "name"=> "dummy title","description_en"=>"dummy description en","description_ja"=>"dummy description ja"])
 <div class='w-full md:w-2/4'>
     <div class='xs:mt-4 px-4 lg:px-0 text-[2.5rem] xl:text-[5rem] leading-[2.5rem] xl:leading-[5rem] lg:tracking-tight font-bold uppercase'>
-        {{ $name }}
+        {{ $event->name }}
     </div>
-    <div class='px-4 lg:px-0 mt-4 lg:hover:scale-105 lg:text-lg transition-all max-h-[18vh] md:h-[30vh] overflow-y-scroll'>
-        {!! $description_en !!}
+    <div class='px-4 lg:px-0 mt-4 lg:hover:scale-105 lg:text-lg transition-all max-h-[18vh] md:h-[30vh] overflow-y-scroll formated-content'>
+        {!! $event->description_en !!}
     </div>
-    <div class='px-4 lg:px-0 mt-4 lg:hover:scale-105 lg:text-lg transition-all max-h-[18vh] md:h-[30vh] overflow-y-scroll'>
-        {!! $description_ja !!}
+    <div class='px-4 lg:px-0 mt-4 lg:hover:scale-105 lg:text-lg transition-all max-h-[18vh] md:h-[30vh] overflow-y-scroll formated-content'>
+        {!! $event->description_ja !!}
     </div>
 </div>
--- resources/views/front/event-show.blade.php
+++ resources/views/front/event-show.blade.php
@@ -1,6 +1,6 @@
 <x-public-layout>
 
-    <div class="h-full">
+    <div class="h-screen flex flex-col">
         <header class='bg-slate-700 text-white'>
             event
         </header>
@@ -10,10 +10,10 @@
 
         <main class="flex-1 mt-4 md:mt-0 h-full flex flex-col md:flex-row items-center justify-evenly" :class="mode === 'default' ? '' : 'hidden'">
             <div class=' md:h-full md:w-auto flex flex-row md:flex-col justify-evenly items-center'>
-                <x-event.calendar-card year="{{$event->start_at->format('Y')}}" month="{{$event->start_at->format('M')}}" day="{{$event->start_at->format('d')}}"/>
+                <x-event.calendar-card :year="$event->start_at->format('Y')" :month="$event->start_at->format('M')" :day="$event->start_at->format('d')"/>
                 <x-event.responsive-related-card  media_id="1" topic_id="2" location_id="3"/>
             </div>
-            <x-event.event-info-card name="{{$event->name}}" description_en="{{$event->description_en}}" description_ja="{{$event->description_ja}}" />
+            <x-event.event-info-card :event="$event"/>
         </main>
 
     </div>
